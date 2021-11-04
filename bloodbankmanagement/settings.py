1156e938b13e20c4c31b0363d30d07918c8f4359
your_usernametirohan <tanbinislam009@gmail.com>
2021-11-04 13:47:28 +0600

    Commit

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -25,9 +25,9 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'static')
 SECRET_KEY = '+zy!9k=9pql5gz9bkqjore)k6r!%w0atk(@(!(!zvp5e(t2i8n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','finddonateblood.herokuapp.com']
 
 
 # Application definition
