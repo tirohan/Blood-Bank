331585161a57ba133f42e3aef04a1c36aec28417
your_usernametirohan <tanbinislam009@gmail.com>
2021-11-04 14:37:19 +0600

    m

--- bloodbankmanagement/settings.py
+++ bloodbankmanagement/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '+zy!9k=9pql5gz9bkqjore)k6r!%w0atk(@(!(!zvp5e(t2i8n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','finddonateblood.herokuapp.com']
+ALLOWED_HOSTS = ['localhost','finddonateblood.herokuapp.com']
 
 
 # Application definition
